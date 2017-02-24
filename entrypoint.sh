a68b1b1b34979f3ff5c7d0d7bf679c7f88584fd9
Vilsol <vilsol2000@gmail.com>
2017-02-24 05:19:11 +0000

    Fix normal build

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ MIN_MEMORY=${MIN_MEMORY:=512M}
 MAX_MEMORY=${MAX_MEMORY:=1G}
 SERVER_ARGUMENTS=${SERVER_ARGUMENTS:=$@}
 
-java -Dnetty.epoll.enabled=false -Xms$MIN_MEMORY -Xmx$MAX_MEMORY $JAVA_FLAGS -jar tridentsdk.jar $SERVER_ARGUMENTS $SERVER_FLAGS
+java -Dnetty.epoll.enabled=false -Xms$MIN_MEMORY -Xmx$MAX_MEMORY $JAVA_FLAGS -jar tridentsdk.jar $SERVER_ARGUMENTS -noepoll $SERVER_FLAGS
