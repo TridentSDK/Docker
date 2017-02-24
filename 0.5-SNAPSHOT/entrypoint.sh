a386cb26febeacfd4258611a9660ea341eed8f7e
Vilsol <vilsol2000@gmail.com>
2017-02-24 23:51:33 +0000

    Fix out of scope error

---
+++ 0.5-SNAPSHOT/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+MIN_MEMORY=${MIN_MEMORY:=512M}
+MAX_MEMORY=${MAX_MEMORY:=1G}
+SERVER_ARGUMENTS=${SERVER_ARGUMENTS:=$@}
+
+java -Dnetty.epoll.enabled=false -Xms$MIN_MEMORY -Xmx$MAX_MEMORY $JAVA_FLAGS -jar tridentsdk.jar $SERVER_ARGUMENTS -noepoll $SERVER_FLAGS
